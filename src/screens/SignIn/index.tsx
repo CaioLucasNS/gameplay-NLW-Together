d39d04becd745afeec5e40f9193a8d154479b265
Caio Lucas <lcaio1281@gmail.com>
2021-06-23 15:35:38 -0300

    criando e estilizando o SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,23 +1,42 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { 
+    View, 
+    Text, 
+    TextInput, 
+    Image,
+    StatusBar
+} from 'react-native';
 
+import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles';
 
 export function SignIn() {
-    const [text, setText] = useState('')
-
     return (
         <View style={styles.container}>
-            <Text>Hello World, NLW Together</Text>
-
-            <TextInput
-                style={styles.input}
-                onChangeText={setText}
+            <StatusBar 
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
             />
 
-            <Text>
-                Você digitou: {text}
-            </Text>
+            <Image 
+                source={IllustrationImg} 
+                style={styles.image}
+                resizeMode="stretch" 
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {`\n`}
+                    suas jogatinas {`\n`}
+                    facilmente
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+            </View>
         </View>
     );
 }
